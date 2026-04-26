e60866d1185537fd50c5445fae68bd2811f02ce4
Joel Miller <millerjoel7597@gmail.com>
2026-04-26 23:32:42 +0100

    refactor: move validators unit test to src/tests/unit

--- src/tests/unit/validators.test.ts
+++ src/tests/unit/validators.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { validateHabitName } from '../../src/lib/validators';
+import { validateHabitName } from '../../lib/validators';
 
 describe('validateHabitName', () => {
   it('returns an error when habit name is empty', () => {
